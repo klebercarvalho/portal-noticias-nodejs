e67147f8709aa718f4c8018ec3fa7fd079352240
Kleber Carvalho <kleber.carvalho@gmail.com>
2018-09-04 14:08:07 -0300

    initial release

---
+++ config/dbConnection.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+
+var connMySQL = function() {
+    // console.log('Conexão com DB foi estabelecida');
+    return mysql.createConnection({
+        host : 'localhost',
+        user : 'root',
+        password : '',
+        database : 'portal_noticias'
+    });
+}
+
+module.exports = function(app) {
+    // console.log('O autoload carregou o módulo de conexão com o DB')
+    return connMySQL;
+};
+
